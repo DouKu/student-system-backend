d3695c856c01c69798fae3dc9497fbfe0f37d587
lgybetter <437675103@qq.com>
2019-05-17 23:58:31 +0800

    :art: 修改port

--- config/config.default.js
+++ config/config.default.js
@@ -52,5 +52,13 @@ module.exports = appInfo => {
     },
   };
 
+  config.cluster = {
+    listen: {
+      path: '',
+      port: 7002,
+      hostname: '127.0.0.1',
+    },
+  };
+
   return config;
 };
